bc3bed2e2fc5327f7314fbfe148458d3c9111499
Godwin Maga <godwinmaga6@gmail.com>
2020-10-06 16:11:22 +0100

    Update product.service.ts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -5,7 +5,7 @@ import { Http, Response } from '@angular/http';
 @Injectable()
 export class ProductService {
 
-  private _albumUrl = '../assests/album.json';
+  private _albumUrl = '../assets/album.json';
 
   constructor(private _http: Http) { }
 
