47d5b18df93303bb83d92f5081f62a07294f1cac
Godwin Maga <godwinmaga6@gmail.com>
2020-10-06 16:00:46 +0100

    Update product.service.ts
    Importing Http And Response Into ProductService and Injecting A Private Http Object In ProductService

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Response } from '@angular/http';
+
 
 @Injectable()
 export class ProductService {
 
-  constructor() { }
+  constructor(private _http: Http) { }
 
 }
