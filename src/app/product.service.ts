727f3d765f1167f286fd1cb92ab40474331eaa1e
Godwin Maga <godwinmaga6@gmail.com>
2020-10-15 11:55:21 +0100

    Update product.service.ts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,7 +9,7 @@ import { Product } from './product';
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
-  private _productsUrl = '../assets/products/json';
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
@@ -20,6 +20,6 @@ export class ProductService {
 
     getProducts(): Observable<Product[]> {
       return this._http.get(this._productsUrl).map(response =>
-        <Product[]>response.json());
+      <Product[]>response.json());
     }
 }  
